b5a188d484945e0e7132ceaa30fa995a8f6b9b72
Nikita Isupov <145199579+nifreebie@users.noreply.github.com>
2025-03-03 01:36:17 +0300

    feat: add bot global exception handler

---
+++ bot/src/main/java/backend/academy/bot/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package backend.academy.bot.exceptionHandler;
+
+import backend.academy.bot.dto.response.ApiErrorResponse;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ApiErrorResponse(
+                        "Некорректные параметры запроса",
+                        e.getErrorCode(),
+                        e.getClass().getSimpleName(),
+                        e.getMessage(),
+                        Stream.of(e.getStackTrace())
+                                .map(StackTraceElement::toString)
+                                .collect(Collectors.toList())));
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<ApiErrorResponse> handleMissingRequest(MissingRequestHeaderException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ApiErrorResponse(
+                        "Некорректные параметры запроса",
+                        "400",
+                        e.getClass().getSimpleName(),
+                        e.getMessage(),
+                        Stream.of(e.getStackTrace())
+                                .map(StackTraceElement::toString)
+                                .collect(Collectors.toList())));
+    }
+}
