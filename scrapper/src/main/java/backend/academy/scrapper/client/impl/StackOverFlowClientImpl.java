5bc61af188e4550b0f026a00b8eef8a379482561
Nikita Isupov <145199579+nifreebie@users.noreply.github.com>
2025-03-03 01:14:11 +0300

    refactor: stackOverFlow client

--- scrapper/src/main/java/backend/academy/scrapper/client/impl/StackOverFlowClientImpl.java
+++ scrapper/src/main/java/backend/academy/scrapper/client/impl/StackOverFlowClientImpl.java
@@ -35,40 +35,40 @@ public class StackOverFlowClientImpl implements StackOverFlowClient {
     public Mono<String> getQuestionLastUpdated(String id) {
         log.info("Fetching last activity date for StackOverflow question: {}", id);
         return webClient
-            .get()
-            .uri(uriBuilder -> uriBuilder
-                .path("/questions/{id}")
-                .queryParam("order", "desc")
-                .queryParam("sort", "activity")
-                .queryParam("site", "stackoverflow")
-                .queryParam("key", key)
-                .queryParam("access_token", accessToken)
-                .build(id))
-            .retrieve()
-            .onStatus(HttpStatus.BAD_REQUEST::equals, response -> {
-                log.warn("Bad request for question ID: {}", id);
-                return response.bodyToMono(String.class)
-                    .flatMap(errorBody -> Mono.error(new RuntimeException("Bad request: " + errorBody)));
-            })
-            .onStatus(HttpStatus.NOT_FOUND::equals, response -> {
-                log.warn("Question not found: {}", id);
-                return response.bodyToMono(String.class)
-                    .flatMap(errorBody -> Mono.error(new RuntimeException("Not found: " + errorBody)));
-            })
-            .onStatus(HttpStatus.INTERNAL_SERVER_ERROR::equals, response -> {
-                log.error("Internal server error while fetching question: {}", id);
-                return response.bodyToMono(String.class)
-                    .flatMap(errorBody ->
-                        Mono.error(new RuntimeException("Internal server error: " + errorBody)));
-            })
-            .bodyToMono(Map.class)
-            .map(response -> {
-                List<?> items = (List<?>) response.get("items");
-                Map<String, ?> map = (Map<String, ?>) items.getFirst();
-                return map.get("last_activity_date").toString();
-            })
-            .doOnSuccess(lastUpdated -> log.info("Question {} last updated at: {}", id, lastUpdated))
-            .doOnError(error ->
-                log.error("Error fetching last activity date for question {} - {}", id, error.getMessage()));
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/questions/{id}")
+                        .queryParam("order", "desc")
+                        .queryParam("sort", "activity")
+                        .queryParam("site", "stackoverflow")
+                        .queryParam("key", key)
+                        .queryParam("access_token", accessToken)
+                        .build(id))
+                .retrieve()
+                .onStatus(HttpStatus.BAD_REQUEST::equals, response -> {
+                    log.warn("Bad request for question ID: {}", id);
+                    return response.bodyToMono(String.class)
+                            .flatMap(errorBody -> Mono.error(new RuntimeException("Bad request: " + errorBody)));
+                })
+                .onStatus(HttpStatus.NOT_FOUND::equals, response -> {
+                    log.warn("Question not found: {}", id);
+                    return response.bodyToMono(String.class)
+                            .flatMap(errorBody -> Mono.error(new RuntimeException("Not found: " + errorBody)));
+                })
+                .onStatus(HttpStatus.INTERNAL_SERVER_ERROR::equals, response -> {
+                    log.error("Internal server error while fetching question: {}", id);
+                    return response.bodyToMono(String.class)
+                            .flatMap(errorBody ->
+                                    Mono.error(new RuntimeException("Internal server error: " + errorBody)));
+                })
+                .bodyToMono(Map.class)
+                .map(response -> {
+                    List<?> items = (List<?>) response.get("items");
+                    Map<String, ?> map = (Map<String, ?>) items.getFirst();
+                    return map.get("last_activity_date").toString();
+                })
+                .doOnSuccess(lastUpdated -> log.info("Question {} last updated at: {}", id, lastUpdated))
+                .doOnError(error ->
+                        log.error("Error fetching last activity date for question {} - {}", id, error.getMessage()));
     }
 }
