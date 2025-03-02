b1c8d4f103bef62cea3c45d3deb7a4078c2c8d6b
Nikita Isupov <145199579+nifreebie@users.noreply.github.com>
2025-03-03 01:26:08 +0300

    feat: add bot utils

---
+++ bot/src/main/java/backend/academy/bot/util/LinkFormatter.java
@@ -0,0 +1,24 @@
+package backend.academy.bot.util;
+
+import backend.academy.bot.dto.response.LinkResponse;
+import backend.academy.bot.dto.response.ListLinksResponse;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class LinkFormatter {
+    public static String formatLinks(ListLinksResponse response) {
+        StringBuilder formatted = new StringBuilder();
+        formatted.append("Ваши ссылки: ").append("\n");
+
+        for (LinkResponse link : response.links()) {
+            formatted.append("Ссылка: ").append(link.url()).append("\n");
+            formatted.append("Тэги: ").append(String.join(", ", link.tags())).append("\n");
+            formatted
+                    .append("Фильтры: ")
+                    .append(String.join(", ", link.filters()))
+                    .append("\n");
+            formatted.append("------------------------------------\n");
+        }
+        return formatted.toString();
+    }
+}
